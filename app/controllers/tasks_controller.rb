7e76d06157f243f05ff8a22c0666f433def0f78a
Tatsuk1 <t.tatsuki0417@gmail.com>
2019-07-01 09:50:49 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
@@ -12,14 +14,13 @@ class TasksController < ApplicationController
   end
   
   def create
-   @task = Task.new(task_params) 
-   
+   @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = "Taskが正常に投稿されました"
-      redirect_to @task
+      flash[:success] = "Taskを投稿しました"
+      redirect_to root_url
     else
-      flash.now[:danger] = "Taskが投稿されませんでした"
-      render :new
+      flash.now[:danger] = "Taskの投稿に失敗しました。"
+      render 'toppages/index'
     end
   end
   
@@ -40,8 +41,8 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     
-    flash[:success] = "Taskは正常に削除されました"
-    redirect_to tasks_url
+    flash[:success] = "Taskを削除しました"
+    redirect_back(fallback_location: root_path)
   end
 
   private
@@ -53,4 +54,11 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_back root_url
+    end
+  end  
 end
